b573ae777a7ff536aa44de9adb62c8a79d2f699d
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2021-09-28 19:38:28 +0530

    Create 1. Obstacle Avoiding.ino

---
+++ 2. Vehicle Control/4 Wheel Drive/1. Obstacle Avoiding.ino
@@ -0,0 +1,66 @@
+#include "AFMotor.h"
+
+#define echopin A3 // echo pin  --- Change these two numbers to respective numbers those are connected in Arduino.
+#define trigpin A4 // Trigger pin
+
+ 
+
+AF_DCMotor motor1(2, MOTOR12_64KHZ); // create motor object, 64KHz pwm
+AF_DCMotor motor2(3, MOTOR12_64KHZ); // create motor object, 64KHz pwm
+AF_DCMotor motor3(1, MOTOR12_64KHZ); // create motor object, 64KHz pwm
+AF_DCMotor motor4(4, MOTOR12_64KHZ); // create motor object, 64KHz pwm
+
+
+int distance;
+long duration;
+
+int set = 15;
+ 
+void setup() {
+  Serial.begin(9600);           // Initialize serial port
+  Serial.println("Start");
+
+  pinMode (trigpin, OUTPUT);
+  pinMode (echopin, INPUT );
+  
+  motor1.setSpeed(255);          // set the motor speed to 0-255
+  motor2.setSpeed(255);
+  motor3.setSpeed(255);          // set the motor speed to 0-255
+  motor4.setSpeed(255);
+
+}
+
+void loop() {
+ distance = data();
+ Serial.print("S=");
+ Serial.println(distance);
+  if (distance > set){
+    Serial.println("Forward");
+    motor1.run(FORWARD);         // turn it on going forward
+    motor2.run(FORWARD);
+    motor3.run(FORWARD);         // turn it on going forward
+    motor4.run(FORWARD); 
+    }
+  else{
+     motor1.run(BACKWARD);   // turn it on going left
+     motor2.run(BACKWARD);
+     motor3.run(BACKWARD);   // turn it on going left
+     motor4.run(BACKWARD); 
+     delay(600);
+     motor1.run(BACKWARD);   // turn it on going left
+     motor2.run(FORWARD);
+     motor3.run(BACKWARD);   // turn it on going left
+     motor4.run(FORWARD);
+     delay(1000); 
+   }
+}
+
+
+long data(){
+  digitalWrite(trigpin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigpin, HIGH);
+  delayMicroseconds(10);
+  duration = pulseIn (echopin, HIGH);
+  return duration / 29 / 2;
+}
