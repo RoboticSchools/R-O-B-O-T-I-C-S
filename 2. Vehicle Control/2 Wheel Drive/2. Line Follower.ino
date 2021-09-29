1446510eb95043bf372bfb58ddd3fa3d9c099835
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2021-09-29 10:11:50 +0530

    Update 2. Line Follower.ino

--- 2. Vehicle Control/2 Wheel Drive/2. Line Follower.ino
+++ 2. Vehicle Control/2 Wheel Drive/2. Line Follower.ino
@@ -5,14 +5,14 @@
 
 #include <AFMotor.h>
 
-// Change these below Pins to connected pins of IR sensor
+//change these two numbers to respective numbers those are connected in Arduino.
 
 #define lefts A5 
 #define rights A1 
 
 
-AF_DCMotor motor1(2, MOTOR12_8KHZ);   // Here right side motor is connected to M1
-AF_DCMotor motor2(3, MOTOR12_8KHZ);   // Here left side motor is connected to M4
+AF_DCMotor motor1(2, MOTOR12_8KHZ);   // Righthand side Motor
+AF_DCMotor motor2(3, MOTOR12_8KHZ);   // Left hand side Motor
 
 
 void setup() {
@@ -29,39 +29,39 @@ void loop(){
   //printing values of the sensors to the serial monitor
   Serial.println(digitalRead(lefts));
   Serial.println(digitalRead(rights));
-  //line detected by both
+  //when line is not detecting
   if(digitalRead(lefts)==0 && digitalRead(rights)==0){
-    //FORWARD
-    motor1.run(FORWARD);
+    
+    motor1.run(FORWARD);     //FORWARD
     motor1.setSpeed(125);
     motor2.run(FORWARD);
     motor2.setSpeed(125);
 
   }
-  //line detected by left sensor
+  // when right hand side sensor detects the line
   else if(digitalRead(lefts)==0 && digitalRead(rights)==1){
-    //turn left
-    motor1.run(BACKWARD);
+    
+    motor1.run(BACKWARD);     //turn Right
     motor1.setSpeed(125);
     motor2.run(FORWARD);
     motor2.setSpeed(100);
 
 
   }
-  //line detected by right sensor
+  // when left hand side sensor detects the line
   else if(digitalRead(lefts)==1 && digitalRead(rights)==0){;
-    //turn left
-    motor1.run(FORWARD);
+    
+    motor1.run(FORWARD);      //turn Right
     motor1.setSpeed(100);
     motor2.run(BACKWARD);
     motor2.setSpeed(125);
 
 
   }
-  //line detected by none
+  // line detected by none
   else if(digitalRead(lefts)==1 && digitalRead(rights)==1){
-    //stop
-    motor1.run(RELEASE);
+    
+    motor1.run(RELEASE);        //stop
     motor2.run(RELEASE);
 
   }
