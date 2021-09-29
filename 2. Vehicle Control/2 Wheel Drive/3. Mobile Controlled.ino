e8aa96cf00ddc532655b803b907421840ca8d559
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2021-09-29 10:16:29 +0530

    Update 3. Mobile Controlled.ino

--- 2. Vehicle Control/2 Wheel Drive/3. Mobile Controlled.ino
+++ 2. Vehicle Control/2 Wheel Drive/3. Mobile Controlled.ino
@@ -5,11 +5,11 @@ const int MOTOR_3 = 3;
  
 
 
-AF_DCMotor motor2(MOTOR_2, MOTOR12_64KHZ); // create motor object, 64KHz pwm
-AF_DCMotor motor3(MOTOR_3, MOTOR12_64KHZ); // create motor object, 64KHz pwm
+AF_DCMotor motor2(MOTOR_2, MOTOR12_64KHZ); // Right Hand side Motor
+AF_DCMotor motor3(MOTOR_3, MOTOR12_64KHZ); // Left Hand side Motor
 
 int state;
-int Speed = 130; 
+int Speed = 180;       // Initial speed of Vehicle
 
 void setup() {
   // set the motor speed to 0-255
@@ -33,8 +33,8 @@ motor3.setSpeed(Speed);
 //===============================================================================
 //                          Key Control Command
 //===============================================================================   
-     if(state == 1){forword(); }  // if the state is '1' the DC motor will go forward
-else if(state == 2){backword();}  // if the state is '2' the motor will Reverse
+     if(state == 1){forward(); }  // if the state is '1' the DC motor will go forward
+else if(state == 2){backward();}  // if the state is '2' the motor will Reverse
 else if(state == 3){turnLeft();}  // if the state is '3' the motor will turn left
 else if(state == 4){turnRight();} // if the state is '4' the motor will turn right
 else if(state == 5){Stop(); }     // if the state is '5' the motor will Stop
@@ -48,37 +48,37 @@ else if(state == 7){turnRight(); delay(400);  state = 5;}
 delay(80);    
 }
 
-void forword(){
- // turn it on going forward
-motor2.run(FORWARD); 
+void forward(){
+ 
+motor2.run(FORWARD);    // move forward
 motor3.run(FORWARD); 
 
 }
 
-void backword(){
- // the other way
-motor2.run(BACKWARD);
+void backward(){
+
+motor2.run(BACKWARD);    // move backward
 motor3.run(BACKWARD); 
  
 }
 
 void turnRight(){
-// the other right
-motor2.run(BACKWARD); 
+
+motor2.run(BACKWARD);     // Turn Right
 motor3.run(FORWARD); 
 
 }
 
 void turnLeft(){
- // turn it on going left
-motor2.run(FORWARD);
+
+motor2.run(FORWARD);       // Turn Left
 motor3.run(BACKWARD); 
 
 }
 
 void Stop(){
- // stopped
- motor2.run(RELEASE);
+
+ motor2.run(RELEASE);      // Stop
  motor3.run(RELEASE);
 
 }
