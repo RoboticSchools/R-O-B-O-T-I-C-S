0e5f4cf7e01e2138d74ddc00d7f339817c1e1ee5
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2021-10-07 15:53:35 +0530

    Update 2. Led Brightness Adjuster.ino

--- 1. Led Blinking using App/2. Led Brightness Adjuster.ino
+++ 1. Led Blinking using App/2. Led Brightness Adjuster.ino
@@ -1,19 +1,29 @@
-int led = 7;
-int data = 0;
+/* 
+    Created by RoboticSchools
+    ------------------------------------------------------------------
+    Robotics and Coding are always fun when we are doing with our Hands
+                  
+                  WELCOME TO THE ROBOTEER'S WORLD
+    ------------------------------------------------------------------
+    Project Name : LED Control using Android App
+    This program helps you to control a LED on pin 13 of arduino using a bluetooth module
+ */
+int led = 7;            // Initilizing the Led pin
+int data = 0;           // Storing data coming from Android App
 
 void setup() {
-  // put your setup code here, to run once:
+                        // put your setup code here, to run once:
 
 Serial.begin(9600);
-pinMode(led,OUTPUT);
+pinMode(led,OUTPUT);    // Set the Led as a output
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-if (Serial.available()>0){
-  data = Serial.read();
-  Serial.println(data);
-  analogWrite(led,data);
+                                  // put your main code here, to run repeatedly:
+if (Serial.available()>0){        // Checking Incoming data from App
+  data = Serial.read();           // Storing data coming from App
+  Serial.println(data);           // Prints the Incoming values coming from App 
+  analogWrite(led,data);          // It Changes the Brightness of Led using analogRead function
 }
 }
