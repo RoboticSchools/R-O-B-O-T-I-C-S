5fb295c3097398d379b6c21ce8b45dadb90b3e26
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2021-10-07 15:42:47 +0530

    Update 1. Led ON OFF Controller.ino

--- 1. Led Blinking using App/1. Led ON OFF Controller.ino
+++ 1. Led Blinking using App/1. Led ON OFF Controller.ino
@@ -4,7 +4,7 @@
  *  Website - www.roboticschools.com
  *  This program helps you to control a LED on pin 13 of arduino using a bluetooth module
  */
-char Incoming_value = 0;                //Variable for storing Incoming_value
+char data = 0;                //Variable for storing Incoming_value from Android App
 void setup() 
 {
   Serial.begin(9600);         //Sets the data rate in bits per second (baud) for serial data transmission
@@ -14,13 +14,13 @@ void loop()
 {
   if(Serial.available() > 0)  
   {
-    Incoming_value = Serial.read();      //Read the incoming data and store it into variable Incoming_value
-    Serial.print(Incoming_value);        //Print Value of Incoming_value in Serial monitor
-    Serial.print("\n");        //New line 
-    if(Incoming_value == '1')            //Checks whether value of Incoming_value is equal to 1 
-      digitalWrite(13, HIGH);  //If value is 1 then LED turns ON
-    else if(Incoming_value == '0')       //Checks whether value of Incoming_value is equal to 0
-      digitalWrite(13, LOW);   //If value is 0 then LED turns OFF
+    data= Serial.read();                 //Read the incoming data and store it into variable Incoming_value
+    Serial.print(data);                  //Print Value of Incoming_value in Serial monitor
+    Serial.print("\n");                  //New line 
+    if(data == '1')                      //Checks whether value of Incoming_value is equal to 1 
+      digitalWrite(13, HIGH);            //If value is 1 then LED turns ON
+    else if(data == '0')                 //Checks whether value of Incoming_value is equal to 0
+      digitalWrite(13, LOW);             //If value is 0 then LED turns OFF
   }                            
  
 }                 
