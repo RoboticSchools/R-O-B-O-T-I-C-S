e82958baf24375fb7f405577fcd8a3aa9fe31606
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2021-10-18 17:49:45 +0530

    Update 1. Number Key Finder.ino

--- 5. IR Remote/1. Number Key Finder.ino
+++ 5. IR Remote/1. Number Key Finder.ino
@@ -1,19 +1,31 @@
+/* 
+ *  Created by RoboticSchools
+ *  Robotics and Coding always fun when we're doing with our hands.
+ *  " Welcome to the Roboteer's World "
+ *  
+ *  FINDING BUTTON NUMBERS IN IR REMOTE USING ARDUINO CODE
+ */
+
+#include <SPI.h>
 #include <IRremote.h>
 
-int IR_Recv = 2; //IR Receiver Pin 2
+int IR_Recv = 2;            //IR Receiver Pin 2
 IRrecv irrecv(IR_Recv);
 decode_results results;
 
-void setup(){
+void setup()
+{
   pinMode(13, OUTPUT);
-  Serial.begin(9600); //starts serial communication
-  irrecv.enableIRIn(); // Starts the receiver
+  Serial.begin(9600);       //starts serial communication
+  irrecv.enableIRIn();      // Starts the receiver
 }
 
-void loop(){
+void loop()
+{
   if (irrecv.decode(&results))
   {
     long int decCode = results.value;
     Serial.println(decCode);
     irrecv.resume();
   }
+}
