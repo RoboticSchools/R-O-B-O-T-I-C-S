5c216afd7628f8db60c3e4c2196518a00a0425b5
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2021-10-18 17:17:19 +0530

    Update 2. LDR Sensor.ino

--- 3. Sensors/2. LDR Sensor.ino
+++ 3. Sensors/2. LDR Sensor.ino
@@ -1,37 +1,46 @@
+/* 
+ *  Created by RoboticSchools
+ *  Robotics and Coding always fun when we're doing with our hands.
+ *  " Welcome to the Roboteer's World "
+ *  
+ *  LED ON & OFF BASED ON DARK AND BRIGHT SURROUNDINGS USING LDR SENSOR
+ */
+
 const int ledPin = 13;
 
 const int ldrPin = A0;
 
 void setup() {
 
-Serial.begin(9600);
+ Serial.begin(9600);
 
-pinMode(ledPin, OUTPUT);
+ pinMode(ledPin, OUTPUT);
 
-pinMode(ldrPin, INPUT);
+ pinMode(ldrPin, INPUT);
 
 }
 
 void loop() {
 
-int ldrStatus = analogRead(ldrPin);
+ int ldrStatus = analogRead(ldrPin);
 
-if (ldrStatus >= 200) {
+ if (ldrStatus >= 200) {
 
-digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin, HIGH);
 
-Serial.print("Its DARK, Turn on the LED : ");
+  Serial.print("Its DARK, Turn on the LED : ");
 
-Serial.println(ldrStatus);
+  Serial.println(ldrStatus);
 
-} else {
+ } 
+ else {
 
-digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin, LOW);
 
-Serial.print("Its BRIGHT, Turn off the LED : ");
+  Serial.print("Its BRIGHT, Turn off the LED : ");
 
-Serial.println(ldrStatus);
+  Serial.println(ldrStatus);
 
-}
+ }
 
 }
