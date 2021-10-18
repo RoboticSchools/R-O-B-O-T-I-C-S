cc33369d8c212a6c07018b2bdcd93bd06958d33f
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2021-10-18 17:50:38 +0530

    Update 2. IR Remote Code.ino

--- 5. IR Remote/2. IR Remote Code.ino
+++ 5. IR Remote/2. IR Remote Code.ino
@@ -1,19 +1,31 @@
+/* 
+ *  Created by RoboticSchools
+ *  Robotics and Coding always fun when we're doing with our hands.
+ *  " Welcome to the Roboteer's World "
+ *  
+ *  TURNING LED ON & OFF USING IR REMOTE
+ */
+
 #include <IRremote.h>
 
 int IR_Recv = 2; //IR Receiver Pin 2
 IRrecv irrecv(IR_Recv);
 decode_results results;
 
-void setup(){
+void setup()
+{
   pinMode(13, OUTPUT);
   Serial.begin(9600); //starts serial communication
   irrecv.enableIRIn(); // Starts the receiver
 }
 
-void loop(){
-  if (irrecv.decode(&results)){
+void loop()
+{
+  if (irrecv.decode(&results))
+  {
     long int decCode = results.value;
     Serial.println(decCode);
+    
     if (results.value == 16753245) //Select button
     {
       digitalWrite(13, HIGH);
