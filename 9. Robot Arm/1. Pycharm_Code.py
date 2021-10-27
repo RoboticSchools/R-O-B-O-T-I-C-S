c80db596272994118f2f7741179cbc5812a5509e
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2021-10-27 11:38:06 +0530

    Create 1. Pycharm_Code.py

---
+++ 9. Robot Arm/1. Pycharm_Code.py
@@ -0,0 +1,19 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from cvzone.SerialModule import SerialObject
+
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(detectionCon=0.8, maxHands=2)
+mySerial = SerialObject("COM3", 9600,1)
+while True:
+
+    success,img = cap.read()
+    hands, img = detector.findHands(img)
+    cv2.imshow("Image", img)
+    if hands:
+        hand1 = hands[0]
+        fingers1 = detector.fingersUp(hand1)
+        #print(fingers1)
+        mySerial.sendData(fingers1)
+    cv2.waitKey(1)
