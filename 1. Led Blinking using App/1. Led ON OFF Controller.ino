4ad15cfd8b84a9da3df4b766031240578608b779
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2021-12-06 12:38:56 +0530

    Update 1. Led ON OFF Controller.ino

--- 1. Led Blinking using App/1. Led ON OFF Controller.ino
+++ 1. Led Blinking using App/1. Led ON OFF Controller.ino
@@ -8,17 +8,21 @@
     Project Name : LED Control using Android App
     This program helps you to control a LED on pin 13 of arduino using a bluetooth module
  */
+#include<SoftwareSerial.h>
+SoftwareSerial mySerial(2,3);
+
 char data = 0;                //Variable for storing Incoming_value from Android App
 void setup() 
 {
-  Serial.begin(9600);         //Sets the data rate in bits per second (baud) for serial data transmission
+  Serial.begin(9600);
+  mySerial.begin(9600);         //Sets the data rate in bits per second (baud) for serial data transmission
   pinMode(13, OUTPUT);        //Sets digital pin 13 as output pin
 }
 void loop()
 {
-  if(Serial.available() > 0)  
+  if(mySerial.available() > 0)  
   {
-    data= Serial.read();                 //Read the incoming data and store it into variable Incoming_value
+    data= mySerial.read();                 //Read the incoming data and store it into variable Incoming_value
     Serial.print(data);                  //Print Value of Incoming_value in Serial monitor
     Serial.print("\n");                  //New line 
     if(data == '1')                      //Checks whether value of Incoming_value is equal to 1 
@@ -27,4 +31,4 @@ void loop()
       digitalWrite(13, LOW);             //If value is 0 then LED turns OFF
   }                            
  
-}                 
+}                           
