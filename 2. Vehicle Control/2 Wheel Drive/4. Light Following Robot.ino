f63332bc790abae62bb58298464cdad619ed0cbb
RoboticSchools <91181914+RoboticSchools@users.noreply.github.com>
2021-12-06 14:23:02 +0530

    Update 4. Light Following Robot.ino

--- 2. Vehicle Control/2 Wheel Drive/4. Light Following Robot.ino
+++ 2. Vehicle Control/2 Wheel Drive/4. Light Following Robot.ino
@@ -6,10 +6,11 @@
  *  LIGHT FOLLOWING ROBOT
  */
 
+
 #include<AFMotor.h> 
-#define L1 A0 
-#define M1 A1
-#define R1 A2
+#define L1 A5 
+
+#define R1 A0
 
 AF_DCMotor motor1(2);
 AF_DCMotor motor2(3);
@@ -20,45 +21,46 @@ void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
 pinMode(L1, INPUT);
-pinMode(M1, INPUT);
+
 pinMode(R1, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 int LSensor = digitalRead(L1);
-int MSensor = digitalRead(M1);
+
 int RSensor = digitalRead(R1);
 
 Serial.print("LSensor");
 Serial.println(LSensor);
-Serial.print("MSensor");
-Serial.println(MSensor);
+
 Serial.print("RSensor");
 Serial.println(RSensor);
 
-if((LSensor == 0)&&(MSensor == 0)&&(RSensor ==0)) { 
+if((LSensor == 0)&&(RSensor ==0)) { 
   //MOVE FORWARD
-  motor1.setSpeed(120);
+  motor1.setSpeed(200);
   motor1.run(FORWARD);
-  motor2.setSpeed(120);
-  motor2.run(FORWARD);
-  
-} else if((LSensor == 0)&&(MSensor == 0)&&(RSensor ==1)) {
-  //TURN LEFT
-  motor1.setSpeed(150);
-  motor1.run(BACKWARD);
-  motor2.setSpeed(150);
-  motor2.run(BACKWARD);
-  
-}else if((LSensor == 1)&&(MSensor == 0)&&(RSensor ==0)) {
-  //TURN RIGHT
-  motor1.setSpeed(150);
-  motor1.run(FORWARD);
-  motor2.setSpeed(150);
+  motor2.setSpeed(200);
   motor2.run(FORWARD);
 
-}else if((LSensor == 1)&&(MSensor == 1)&&(RSensor ==1)) {
+  
+} else if((LSensor == 0)&&(RSensor ==1)) {
+  //TURN LEFT
+  motor1.setSpeed(200);
+  motor1.run(FORWARD);
+  motor2.setSpeed(200);
+  motor2.run(BACKWARD);
+
+  
+}else if((LSensor == 1)&&(RSensor ==0)) {
+  //TURN RIGHT
+  motor1.setSpeed(200);
+  motor1.run(BACKWARD);
+  motor2.setSpeed(200);
+  motor2.run(FORWARD);
+
+}else if((LSensor == 1)&&(RSensor ==1)) {
   //STOP
   motor1.setSpeed(0);
   motor1.run(RELEASE);
